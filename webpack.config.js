303ae6be5880d4a32c61b24c8a7df7ef74e3c50c
Jordan Lane <jordan.lane@veerum.com>
2024-12-18 17:17:45 -0800

    fix: properly export index files

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'potree.cjs',
+    filename: 'index.cjs',
     library: {
       type: 'umd',
       umdNamedDefine: true,
--- webpack.esm.config.js
+++ webpack.esm.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'potree.mjs',
+    filename: 'index.mjs',
     module: true,
     library: {
       type: 'module',
