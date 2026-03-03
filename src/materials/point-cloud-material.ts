5dc4d7ddfc2816d8a5da9518a9c98e1fe1ecec00
Jordan Lane <jordan.lane@veerum.com>
2026-03-03 10:49:18 -0700

    chore: upgrade threejs to 0.180.0

--- src/materials/point-cloud-material.ts
+++ src/materials/point-cloud-material.ts
@@ -353,9 +353,9 @@ export class PointCloudMaterial extends RawShaderMaterial {
 
     this.classification = DEFAULT_CLASSIFICATION;
 
-    this.defaultAttributeValues.normal = [0, 0, 0];
-    this.defaultAttributeValues.classification = [0, 0, 0];
-    this.defaultAttributeValues.indices = [0, 0, 0, 0];
+    // this.defaultAttributeValues.normal = [0, 0, 0];
+    // this.defaultAttributeValues.classification = [0, 0, 0];
+    // this.defaultAttributeValues.indices = [0, 0, 0, 0];
 
     this.vertexColors = true;
 
@@ -486,8 +486,8 @@ export class PointCloudMaterial extends RawShaderMaterial {
       define('color_rgba');
     }
 
-    if(this.hqDepthPass) {
-      define('hq_depth_pass')
+    if (this.hqDepthPass) {
+      define('hq_depth_pass');
     }
 
     define('MAX_POINT_LIGHTS 0');
@@ -692,15 +692,15 @@ export class PointCloudMaterial extends RawShaderMaterial {
       }
 
       const density = (node.geometryNode as any).density;
-      if(density && typeof density == 'number' && !Number.isNaN(density)){
-				let lodOffset = Math.log2(density) / 2 - 1.5;
+      if (density && typeof density == 'number' && !Number.isNaN(density)) {
+        let lodOffset = Math.log2(density) / 2 - 1.5;
 
-				let offsetUint8 = (lodOffset + 10) * 10;
+        let offsetUint8 = (lodOffset + 10) * 10;
 
-				data[i * 4 + 3] = offsetUint8;
-			} else {
-				data[i * 4 + 3] = 100;
-			}
+        data[i * 4 + 3] = offsetUint8;
+      } else {
+        data[i * 4 + 3] = 100;
+      }
       // data[i * 4 + 3] = node.name.length;
     }
 
