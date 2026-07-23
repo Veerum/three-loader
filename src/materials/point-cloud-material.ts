81d9d59d1f0640070a5b79c03380e6ed6b5da75c
Riley Munro <riley.munro@veerum.com>
2026-07-23 10:09:59 -0600

    chore: restore formatting

--- src/materials/point-cloud-material.ts
+++ src/materials/point-cloud-material.ts
@@ -495,8 +495,8 @@ export class PointCloudMaterial extends RawShaderMaterial {
       define('color_rgba');
     }
 
-    if (this.hqDepthPass) {
-      define('hq_depth_pass');
+    if(this.hqDepthPass) {
+      define('hq_depth_pass')
     }
 
     define('MAX_POINT_LIGHTS 0');
@@ -701,15 +701,15 @@ export class PointCloudMaterial extends RawShaderMaterial {
       }
 
       const density = (node.geometryNode as any).density;
-      if (density && typeof density == 'number' && !Number.isNaN(density)) {
-        let lodOffset = Math.log2(density) / 2 - 1.5;
+      if(density && typeof density == 'number' && !Number.isNaN(density)){
+				let lodOffset = Math.log2(density) / 2 - 1.5;
 
-        let offsetUint8 = (lodOffset + 10) * 10;
+				let offsetUint8 = (lodOffset + 10) * 10;
 
-        data[i * 4 + 3] = offsetUint8;
-      } else {
-        data[i * 4 + 3] = 100;
-      }
+				data[i * 4 + 3] = offsetUint8;
+			} else {
+				data[i * 4 + 3] = 100;
+			}
       // data[i * 4 + 3] = node.name.length;
     }
 
